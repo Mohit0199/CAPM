4b53c4ffd21fe5d76ae0da6697a387fe77917861
Mohit Rathod <137507173+Mohit0199@users.noreply.github.com>
2024-12-08 10:12:19 +0530

    Update Stock_Prediction.py

--- pages/Stock_Prediction.py
+++ pages/Stock_Prediction.py
@@ -17,28 +17,32 @@ col1, col2, col3 = st.columns(3)
 with col1:
     ticker = st.text_input('Stock Ticker', 'TATAMOTORS.NS')
 
-rmse = 0
+try:
+    rmse = 0
+    
+    st.subheader("Predicting next 30 days closing price for: "+ticker)
+    
+    close_price = get_data(ticker)
+    rolling_price = get_rolling_mean(close_price)
+    
+    differencing_order = get_differencing_order(rolling_price)
+    scaled_data, scaler = scaling(rolling_price)
+    rmse = evaluate_model(scaled_data, differencing_order)
+    
+    st.write("**Model RMSE Score:**", rmse)
+    
+    forecast = get_forecast(scaled_data, differencing_order)
+    forecast['Close'] = inverse_scaling(scaler, forecast['Close'])
+    
+    st.write("##### Forecast Data (Next 30 Days)")
+    
+    fig_tail = plotly_table(forecast.sort_index(ascending=True).round(3))
+    fig_tail.update_layout(height=220)
+    st.plotly_chart(fig_tail, use_container_width=True)
+    
+    forecast = pd.concat([rolling_price, forecast])
+    st.plotly_chart(Moving_average_forecast(forecast.iloc[150:]), use_container_width=True)
 
-st.subheader("Predicting next 30 days closing price for: "+ticker)
-
-close_price = get_data(ticker)
-rolling_price = get_rolling_mean(close_price)
-
-differencing_order = get_differencing_order(rolling_price)
-scaled_data, scaler = scaling(rolling_price)
-rmse = evaluate_model(scaled_data, differencing_order)
-
-st.write("**Model RMSE Score:**", rmse)
-
-forecast = get_forecast(scaled_data, differencing_order)
-forecast['Close'] = inverse_scaling(scaler, forecast['Close'])
-
-st.write("##### Forecast Data (Next 30 Days)")
-
-fig_tail = plotly_table(forecast.sort_index(ascending=True).round(3))
-fig_tail.update_layout(height=220)
-st.plotly_chart(fig_tail, use_container_width=True)
-
-forecast = pd.concat([rolling_price, forecast])
-st.plotly_chart(Moving_average_forecast(forecast.iloc[150:]), use_container_width=True)
+except:
+    st.st.write("Please select valid inputs")
 
